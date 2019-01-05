5363b17c8dfffc8ff9ca52c1d0f036e6ce4a32c6
Trung Pham <trungphamvan2810@gmail.com>
2019-01-05 11:34:37 +0700

    code convention

--- OopPractice/Class/VietnamNet.php
+++ OopPractice/Class/VietnamNet.php
@@ -2,28 +2,28 @@
 include('TakeCode.php');
 class VietnamNet extends TakeCode
 {
-    private $search1 = 'class="title f-22 c-3e">';
-    private $search2 = '<div class="inner-article"';
-    private $search3 = '</h1>';
+    private $__search1 = 'class="title f-22 c-3e">';
+    private $__search2 = '<div class="inner-article"';
+    private $__search3 = '</h1>';
 
-    public function deleteGarbage() {
+    private function __deleteGarbage() {
         $b = parent::takeCodeWebsite();
         // xóa đoạn code bên trên tiêu đề
-        $c = parent::deleteBefore($this->search1, $b);
+        $c = parent::deleteBefore($this->__search1, $b);
         // xóa đoạn code bên dưới nội dung
-        $c = parent::deleteAfter($this->search2, $c);
+        $c = parent::deleteAfter($this->__search2, $c);
         return $c;
     }
 
     public function takeTitle()
     {
-        $f = parent::deleteAfter($this->search3, $this->deleteGarbage());
+        $f = parent::deleteAfter($this->__search3, $this->__deleteGarbage());
         return strip_tags($f);
     }
 
     public function takeContent()
     {   
-        $gString = str_replace( $this->takeTitle(),' ', $this->deleteGarbage());
+        $gString = str_replace( $this->takeTitle(),' ', $this->__deleteGarbage());
         $gString = parent::deleteBefore('class="ArticleContent">', $gString);
         return strip_tags($gString);
     }
