d961905b1f1b462af3d2e4a9f95957abc601a42e
TrungPham <nhokpipuppy@gmail.com>
2019-01-06 13:10:13 +0700

    change user spl_autoload_register for __autoload

--- OopPractice/__autoload.php
+++ OopPractice/__autoload.php
@@ -1,5 +1,14 @@
 <?php 
-function __autoload($class_name){
-   require_once('class/'.$class_name.'.php'); 
-}
+// function __autoload($className){
+   // require_once('class/'.$class_name.'.php'); 
+// }
+// function my_autoloader($class_name) {
+//    // include('class/'.$class_name.'.php');
+//    include 'class/'.$class_name.'.php';
+// }
+
+// spl_autoload_register('my_autoloader');
+spl_autoload_register(function($className) {
+	include_once 'Class/'.$className.'.php';
+});
  ?>
