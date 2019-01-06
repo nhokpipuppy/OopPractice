28ce18e6c29b9c713b81b68abb03bdd27987e9dc
TrungPham <nhokpipuppy@gmail.com>
2019-01-06 13:12:55 +0700

    edit

--- OopPractice/__autoload.php
+++ OopPractice/__autoload.php
@@ -1,13 +1,4 @@
 <?php 
-// function __autoload($className){
-   // require_once('class/'.$class_name.'.php'); 
-// }
-// function my_autoloader($class_name) {
-//    // include('class/'.$class_name.'.php');
-//    include 'class/'.$class_name.'.php';
-// }
-
-// spl_autoload_register('my_autoloader');
 spl_autoload_register(function($className) {
 	include_once 'Class/'.$className.'.php';
 });
