703f2eda2739f955b4caa9d57362e4018d5094e6
TrungPham <nhokpipuppy@gmail.com>
2019-01-08 08:19:54 +0700

    define a const ERROR

--- OopPractice/application/config/config.php
+++ OopPractice/application/config/config.php
@@ -3,4 +3,9 @@
     define('DB_NAME', 'ControlData');
     define('DB_USER', 'root');
     define('DB_PASS', '123456');
+    define('ERROR', '<span>Add data false. Url phải là 1 bài báo trong 2 trang vnexpress.net và vietnamnet.vn</span>
+    <div class="back" style="text-align:center">
+    <a href="index.php">Quay lại trang chủ</a>
+    </div>');
+    
 ?>
